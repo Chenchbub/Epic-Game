64f8dcb4a6ec7ece827565f90ff69c8e2d4f009f
Ryan Christensen <rc25412@ms.wasatch.edu>
2021-04-19 11:34:46 -0600

    Endless Runner
    I have made a small course with obstacles and walls and have made falling tiles and holes in the floor. I have a checkpoint area without any code for checkpoints.

---
+++ Assets/Scripts/FallingTiles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FallingTiles : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.up;
+    public float smooth = 2.0F;
+    public float tiltAngle = 3.0F;
+    void Update()
+    {
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
+    }
+}
