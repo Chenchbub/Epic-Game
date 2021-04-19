a15a51d028db6c732bf6afba440fcc03329805d6
Grant <grantharris0323@gmail.com>
2021-04-19 11:28:06 -0600

    Player Controller
    Replace your input handler, player controller, and controls scripts, if you created them for some reason so that the Player Controller script actually works. If it doesn't work, see me and I will attempt to resolve.

---
+++ Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static InputHandler instance;
+    private Controls controls;
+
+    
+    public Vector2 move;
+    public Vector2 look;
+    public bool pickupDown = false;
+    public bool sprintDown = false;
+    public bool jumpDown = false;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this);
+        } else
+        {
+            instance = this;
+        }
+
+        controls = new Controls();
+    }
+
+    private void OnEnable ()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    } 
+
+    private void Start()
+    { 
+
+        controls.Locomotion.Move.performed += controls => move =controls.ReadValue<Vector2>();
+            
+        
+        controls.Locomotion.Look.performed += controls => look = controls.ReadValue<Vector2>();
+
+        controls.Locomotion.Jump.performed += controls => jumpDown = true;
+        controls.Locomotion.Jump.canceled += controls => jumpDown = false;
+
+        controls.Locomotion.Sprint.performed += controls => sprintDown = true;
+
+        controls.Locomotion.Sprint.canceled += controls => sprintDown = false;
+
+        controls.Locomotion.Pickup.performed += controls =>pickupDown = true;
+        controls.Locomotion.Pickup.canceled += controls => pickupDown = false;
+    }
+}
